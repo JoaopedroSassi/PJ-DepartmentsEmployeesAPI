1dcc7da3aef87d66c70c83994d3386815667375f
João Pedro Sassi Granado <joaopedro10813@gmail.com>
2022-03-02 15:57:47 -0300

    Data configurations

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DepartmentsCompanies.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DepartmentsCompanies.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
