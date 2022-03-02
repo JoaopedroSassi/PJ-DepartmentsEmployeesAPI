098c8179d813fb7e96cf4deddf522c2e3509f01a
João Pedro Sassi Granado <joaopedro10813@gmail.com>
2022-03-02 18:49:33 -0300

    Update name from "Eployee" to "Employee"

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,7 +5,7 @@ namespace DepartmentsCompanies.Data
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<Employee> Eployees { get; set; }
+        public DbSet<Employee> Employees { get; set; }
         public DbSet<Department> Departments { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
---
+++ Migrations/20220302214746_UpdateName.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DepartmentsCompanies.Migrations
+{
+    public partial class UpdateName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Eployees",
+                table: "Eployees");
+
+            migrationBuilder.RenameTable(
+                name: "Eployees",
+                newName: "Employees");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Employees",
+                table: "Employees",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Employees",
+                table: "Employees");
+
+            migrationBuilder.RenameTable(
+                name: "Employees",
+                newName: "Eployees");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Eployees",
+                table: "Eployees",
+                column: "Id");
+        }
+    }
+}
