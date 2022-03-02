6c3183e5574e28aff5008681dd7682e693ad53e0
João Pedro Sassi Granado <joaopedro10813@gmail.com>
2022-03-02 15:28:11 -0300

    Add initial files

---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DepartmentsCompanies.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DepartmentsCompanies.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
